1b988f6dfda0fe55dd71b428f097fb52d4027619
MajharolTanvir <majharul5456@gmail.com>
2023-07-28 16:11:16 +0600

    Gpu page implemented successfully

--- src/pages/categories/gpu.js
+++ src/pages/categories/gpu.js
@@ -1,10 +1,14 @@
 import RootLayout from '@/components/Layouts/RootLayout';
+import ComponentCard from '@/components/Ui/componentCard';
 import React from 'react';
 
-const Gpu = () => {
+const Gpu = ({products}) => {
+    console.log(products)
     return (
-        <div>
-            <h1>This is gpu page</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            {
+                products?.map(product => <ComponentCard key={product.id} product={product} />)
+            }
         </div>
     );
 };
@@ -18,4 +22,16 @@ Gpu.getLayout = function getLayout(page) {
             {page}
         </RootLayout>
     )
+}
+
+export const getStaticProps = async () => {
+    const res = await fetch(`http://localhost:3000/api/products`)
+    const productsData = await res.json()
+    const data = productsData.data.filter(products => products.category === 'GPU')
+
+
+    return {
+        props: { products: data }
+
+    }
 }
